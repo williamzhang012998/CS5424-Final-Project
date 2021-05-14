07b5a28b9767aa9e80fac23dfb48452ccdf1369b
William Zhang <williamzhang012998@gmail.com>
2021-05-13 20:41:47 -0400

    edit

---
+++ main.py
@@ -0,0 +1,487 @@
+import cv2
+import sys
+from mail import sendEmail
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+from flask_basicauth import BasicAuth
+import time
+import threading
+
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
+from datetime import datetime
+from random import randint
+import board
+import busio
+from adafruit_bus_device.i2c_device import I2CDevice
+
+from vosk import Model, KaldiRecognizer
+import sys
+import os
+import wave
+import json
+import subprocess
+import smbus
+import time
+from mpu6050 import *
+glossary = {
+            "money",
+            "purse",
+            "police",
+            "shoot"
+            }
+
+class mpu6050:
+
+    # Global Variables
+    GRAVITIY_MS2 = 9.80665
+    address = None
+    bus = None
+
+    # Scale Modifiers
+    ACCEL_SCALE_MODIFIER_2G = 16384.0
+    ACCEL_SCALE_MODIFIER_4G = 8192.0
+    ACCEL_SCALE_MODIFIER_8G = 4096.0
+    ACCEL_SCALE_MODIFIER_16G = 2048.0
+
+    GYRO_SCALE_MODIFIER_250DEG = 131.0
+    GYRO_SCALE_MODIFIER_500DEG = 65.5
+    GYRO_SCALE_MODIFIER_1000DEG = 32.8
+    GYRO_SCALE_MODIFIER_2000DEG = 16.4
+
+    # Pre-defined ranges
+    ACCEL_RANGE_2G = 0x00
+    ACCEL_RANGE_4G = 0x08
+    ACCEL_RANGE_8G = 0x10
+    ACCEL_RANGE_16G = 0x18
+
+    GYRO_RANGE_250DEG = 0x00
+    GYRO_RANGE_500DEG = 0x08
+    GYRO_RANGE_1000DEG = 0x10
+    GYRO_RANGE_2000DEG = 0x18
+
+    # MPU-6050 Registers
+    PWR_MGMT_1 = 0x6B
+    PWR_MGMT_2 = 0x6C
+
+    ACCEL_XOUT0 = 0x3B
+    ACCEL_YOUT0 = 0x3D
+    ACCEL_ZOUT0 = 0x3F
+
+    TEMP_OUT0 = 0x41
+
+    GYRO_XOUT0 = 0x43
+    GYRO_YOUT0 = 0x45
+    GYRO_ZOUT0 = 0x47
+
+    ACCEL_CONFIG = 0x1C
+    GYRO_CONFIG = 0x1B
+
+    def __init__(self, address, bus=1):
+        self.address = address
+        self.bus = smbus.SMBus(bus)
+        # Wake up the MPU-6050 since it starts in sleep mode
+        self.bus.write_byte_data(self.address, self.PWR_MGMT_1, 0x00)
+
+    # I2C communication methods
+
+    def read_i2c_word(self, register):
+        # Read the data from the registers
+        high = self.bus.read_byte_data(self.address, register)
+        low = self.bus.read_byte_data(self.address, register + 1)
+
+        value = (high << 8) + low
+
+        if (value >= 0x8000):
+            return -((65535 - value) + 1)
+        else:
+            return value
+
+    def set_accel_range(self, accel_range):
+        # First change it to 0x00 to make sure we write the correct value later
+        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, 0x00)
+
+        # Write the new range to the ACCEL_CONFIG register
+        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, accel_range)
+
+    def read_accel_range(self, raw = False):
+        raw_data = self.bus.read_byte_data(self.address, self.ACCEL_CONFIG)
+
+        if raw is True:
+            return raw_data
+        elif raw is False:
+            if raw_data == self.ACCEL_RANGE_2G:
+                return 2
+            elif raw_data == self.ACCEL_RANGE_4G:
+                return 4
+            elif raw_data == self.ACCEL_RANGE_8G:
+                return 8
+            elif raw_data == self.ACCEL_RANGE_16G:
+                return 16
+            else:
+                return -1
+
+    def get_accel_data(self, g = False):
+        x = self.read_i2c_word(self.ACCEL_XOUT0)
+        y = self.read_i2c_word(self.ACCEL_YOUT0)
+        z = self.read_i2c_word(self.ACCEL_ZOUT0)
+
+        accel_scale_modifier = None
+        accel_range = self.read_accel_range(True)
+
+        if accel_range == self.ACCEL_RANGE_2G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
+        elif accel_range == self.ACCEL_RANGE_4G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_4G
+        elif accel_range == self.ACCEL_RANGE_8G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_8G
+        elif accel_range == self.ACCEL_RANGE_16G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_16G
+        else:
+            print("Unknown range-accel_scale_modifier set to self.ACCEL_SCALE_MODIFIER_2G")
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
+
+        x = x / accel_scale_modifier
+        y = y / accel_scale_modifier
+        z = z / accel_scale_modifier
+
+        if g is True:
+            return {'x': x, 'y': y, 'z': z}
+        elif g is False:
+            x = x * self.GRAVITIY_MS2
+            y = y * self.GRAVITIY_MS2
+            z = z * self.GRAVITIY_MS2
+            return {'x': x, 'y': y, 'z': z}
+
+    def set_gyro_range(self, gyro_range):
+        # First change it to 0x00 to make sure we write the correct value later
+        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, 0x00)
+
+        # Write the new range to the ACCEL_CONFIG register
+        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, gyro_range)
+
+    def read_gyro_range(self, raw = False):
+        raw_data = self.bus.read_byte_data(self.address, self.GYRO_CONFIG)
+
+        if raw is True:
+            return raw_data
+        elif raw is False:
+            if raw_data == self.GYRO_RANGE_250DEG:
+                return 250
+            elif raw_data == self.GYRO_RANGE_500DEG:
+                return 500
+            elif raw_data == self.GYRO_RANGE_1000DEG:
+                return 1000
+            elif raw_data == self.GYRO_RANGE_2000DEG:
+                return 2000
+
+            else:
+                return -1
+
+    def get_gyro_data(self):
+        x = self.read_i2c_word(self.GYRO_XOUT0)
+        y = self.read_i2c_word(self.GYRO_YOUT0)
+        z = self.read_i2c_word(self.GYRO_ZOUT0)
+
+        gyro_scale_modifier = None
+        gyro_range = self.read_gyro_range(True)
+
+        if gyro_range == self.GYRO_RANGE_250DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+        elif gyro_range == self.GYRO_RANGE_500DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_500DEG
+        elif gyro_range == self.GYRO_RANGE_1000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_1000DEG
+        elif gyro_range == self.GYRO_RANGE_2000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_2000DEG
+        else:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+
+        x = x / gyro_scale_modifier
+        y = y / gyro_scale_modifier
+        z = self.read_i2c_word(self.GYRO_ZOUT0)
+
+        gyro_scale_modifier = None
+        gyro_range = self.read_gyro_range(True)
+
+        if gyro_range == self.GYRO_RANGE_250DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+        elif gyro_range == self.GYRO_RANGE_500DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_500DEG
+        elif gyro_range == self.GYRO_RANGE_1000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_1000DEG
+        elif gyro_range == self.GYRO_RANGE_2000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_2000DEG
+        else:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+
+        x = x / gyro_scale_modifier
+        y = y / gyro_scale_modifier
+        z = z / gyro_scale_modifier
+
+        return {'x': x, 'y': y, 'z': z}
+
+    def get_all_data(self):
+        temp = self.get_temp()
+        accel = self.get_accel_data()
+        gyro = self.get_gyro_data()
+
+        return [accel, gyro, temp]
+
+mpu = mpu6050(0x68)
+
+
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+def Speech2Text():
+    wf = wave.open("recording.wav", "rb")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print ("Audio file must be WAV format mono PCM.")
+        exit (1)
+
+    model = Model("model1")
+    # You can also specify the possible word list
+    rec = KaldiRecognizer(model, wf.getframerate(), "money purse police shoot")
+
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
+    res = json.loads(rec.FinalResult())
+    print ("Speech2Text: "+ res['text'])
+    return res['text']
+
+def ScaleImage(image):
+# Scale the image to the smaller screen dimension
+	width = 135
+	height = 240
+
+	image_ratio = image.width / image.height
+	screen_ratio = width / height
+	if screen_ratio < image_ratio:
+	    scaled_width = image.width * height // image.height
+	    scaled_height = height
+	else:
+	    scaled_width = width
+	    scaled_height = image.height * width // image.width
+	image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+	# Crop and center the image
+	x = scaled_width // 2 - width // 2
+	y = scaled_height // 2 - height // 2
+	image = image.crop((x, y, x + width, y + height))
+
+	image = image.convert('RGB')
+	image = image.resize((240, 135),Image.BICUBIC)
+	return image
+
+
+if not os.path.exists("model1"):
+    print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
+    exit (1)
+
+# Try to create an I2C device
+i2c = busio.I2C(board.SCL, board.SDA)
+print("I2C ok!")
+# ids = '\n'.join(map(str,i2c.scan()))
+# print(f"I2C device ID's found:\n{ids}")
+
+while not i2c.try_lock():
+    pass
+
+print("I2C addresses found:", [hex(device_address) for device_address in i2c.scan()])
+i2c.unlock()
+
+# prepare the accelerometer
+wrist = False
+
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+# Image Formatting
+def image_formatting(imagef, width, height):
+    imagef = imagef.convert('RGB')
+    imagef = imagef.resize((240, 135), Image.BICUBIC)
+
+    return imagef
+
+listening = False
+speaking = False
+text = ""
+
+if __name__ == "__main__":
+    while (1):
+        try:
+            email_update_interval = 600 # sends an email only once in this time interval
+            video_camera = VideoCamera(flip=True) # creates a camera object, flip vertically
+            object_classifier = cv2.CascadeClassifier("models/facial_recognition_model.xml") # an opencv classifier
+
+            # App Globals (do not edit)
+            app = Flask(__name__)
+            app.config['BASIC_AUTH_USERNAME'] = 'pi'
+            app.config['BASIC_AUTH_PASSWORD'] = 'williamjeanne2021'
+            app.config['BASIC_AUTH_FORCE'] = True
+
+            basic_auth = BasicAuth(app)
+            last_epoch = 0
+
+            image3 = Image.open("911.jpg")
+            image3 = image_formatting(image3, width, height)
+            disp.image(image3, rotation)
+
+            def check_for_objects():
+                global last_epoch
+                while True:
+
+                    try:
+                        frame, found_obj = video_camera.get_object(object_classifier)
+                        if found_obj and (time.time() - last_epoch) > email_update_interval:
+                            last_epoch = time.time()
+                            print ("Sending email...")
+                            sendEmail(frame)
+                            print ("done!")
+
+                        accel_data = mpu.get_accel_data()
+                        gyro_data = mpu.get_gyro_data()
+                        x = accel_data['x']
+                        y = accel_data['y']
+                        z = accel_data['z']
+                        print(x, y, z)
+                        if (abs(z-y) < 2) and (x>9) and (x<11):
+                            wrist = True
+                            print("perpendicular")
+                            print("money")
+
+                        else:
+                            wrist = False
+                            print("not perpendicular")
+
+                        if wrist:
+                            if not listening:
+                                process = subprocess.Popen(["arecord", "-D", "hw:2,0", "-d", "5", "-f", "cd", "recording.wav", "-c", "1"])
+                                listening = True
+                                print("speak!!")
+                        else:
+                            if listening:
+                                process.kill()
+
+                                listening = False
+                                speaking = True
+                                print("stop!!")
+
+                        if speaking:
+                            speaking = False
+                            text = Speech2Text()
+                            print(text)
+                            if text == "money":
+                                image3 = Image.open("911.jpg")
+                                image3 = image_formatting(image3, width, height)
+                                disp.image(image3, rotation)
+                            elif text == "purse":
+                                image3 = Image.open("911.jpg")
+                                image3 = image_formatting(image3, width, height)
+                                disp.image(image3, rotation)
+                            elif text == "police":
+                                image3 = Image.open("911.jpg")
+                                image3 = image_formatting(image3, width, height)
+                                disp.image(image3, rotation)
+                            elif text == "shoot":
+                                image3 = Image.open("911.jpg")
+                                image3 = image_formatting(image3, width, height)
+                                disp.image(image3, rotation)
+                    except:
+                        print ("Error sending email: ", sys.exc_info()[0])
+
+                    time.sleep(3)
+
+            @app.route('/')
+            @basic_auth.required
+            def index():
+                return render_template('index.html')
+
+            def gen(camera):
+                while True:
+                    frame = camera.get_frame()
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+            @app.route('/video_feed')
+            def video_feed():
+                return Response(gen(video_camera),
+                                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+            if __name__ == '__main__':
+                t = threading.Thread(target=check_for_objects, args=())
+                t.daemon = True
+                t.start()
+                app.run(host='0.0.0.0', debug=False)
+
+            
+        except KeyboardInterrupt:
+            break
+
+
+
+
